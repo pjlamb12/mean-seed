8f21e6a62b3703a805eac22ba2b88aadb86eb24a
Preston Lamb <plamb@axis41.com>
2014-12-11 09:06:54 -0700

    Changed console.log of db name to be mean-seed.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error...'));
 	db.once('open', function callback() {
-		console.log('multivision db opened');
+		console.log('mean-seed db opened');
 	});
 
 };
